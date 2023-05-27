7032fd74cf97c04c41e5202e7cfdef6b9f3033e5
zahideaksak <zahide.aksaak@gmail.com>
2023-05-27 22:07:09 +0300

    redux-persistt

---
+++ src/redux/store/index.tsx
@@ -0,0 +1,27 @@
+import {configureStore, combineReducers} from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
+import taskReducer from '../reducers/task';
+import {persistReducer} from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+};
+
+let rootReducer = combineReducers({
+  task: taskReducer,
+});
+
+let persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  // devTools: process.env.NODE_ENV !== 'production',
+  middleware: [thunk],
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
